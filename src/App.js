9b6f16b646ac1dd06529d339f63a444ad4202c69
Christian <gamethree@protonmail.com>
2021-06-16 04:56:31 -0400

    metadata

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          A private NFT Marketplace Launching In
         </a>
       </header>
     </div>
